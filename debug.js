5b569a7cdde2a7f57ac6c23daaf702b0a0556aa9
Samuel Chow <cybersam201004@gmail.com>
2014-08-19 23:12:44 -0700

    Added logic to pre-populate the parameters and support for multipart form-data.

--- debug.js
+++ debug.js
@@ -31,6 +31,12 @@ $("#addfilebutton").click(function(e) {
 	showParamHeaders();
 });
 
+$("#resetfilebutton").click(function(e) {
+  e.preventDefault();
+  initForm("GET", "", {});
+  showParamHeaders();
+});
+
 function postWithAjax(myajax) {
   myajax = myajax || {};
   myajax.url = $("#urlvalue").val();
@@ -47,7 +53,34 @@ function postWithAjax(myajax) {
 		} else {
 			$("#statuspre").addClass("alert-warning");
 		}
-		$("#outputpre").text(jqXHR.responseText);
+
+    var contentType = jqXHR.getResponseHeader('content-type');
+
+    if (contentType.indexOf('application/json') >= 0) {
+      $("#outputpre").show();
+      $("#outputpre").text(jqXHR.responseText);
+      $("#outputiframe").hide();
+      $("#outputiframe").empty();
+      $("#outputimg").hide();
+      $("#outputimg").empty();
+    }
+    else if (contentType.match(/image\/.*/gif)) {
+      $("#outputpre").hide();
+      $("#outputpre").empty();
+      $("#outputiframe").hide();
+      $("#outputiframe").empty();
+      $("#outputimg").show();
+      $("#outputimg").attr("src", myajax.url);  // TODO: Load the image directly to the img element.
+    }
+    else {
+      $("#outputpre").hide();
+      $("#outputpre").empty();
+      $("#outputiframe").show();
+      $("#outputiframe").attr("srcdoc", jqXHR.responseText);
+      $("#outputimg").hide();
+      $("#outputimg").empty();
+    }
+
 		$("#headerpre").text(jqXHR.getAllResponseHeaders());
 	}
 
@@ -56,9 +89,15 @@ function postWithAjax(myajax) {
 	}
 
 	$("#outputframe").hide();
-	$("#outputpre").empty();
+  $("#outputpre").show();
+  $("#outputpre").empty();
+  $("#outputiframe").hide();
+  $("#outputiframe").empty();
+  $("#outputframe").attr("src", "")
+  $("#outputframe").attr("srcdoc", "")
+  $("#outputimg").hide();
+  $("#outputimg").empty();
 	$("#headerpre").empty();
-	$("#outputframe").attr("src", "")
 	$("#ajaxoutput").show();
 	$("#statuspre").text("0");
 	$("#statuspre").removeClass("alert-success");
@@ -94,7 +133,7 @@ function checkForFiles() {
 function createUrlData(){
   var mydata = {};
 	var parameters = $("#allparameters").find(".realinputvalue");
-	for (i = 0; i < parameters.length; i++) {
+	for (var i = 0; i < parameters.length; i++) {
 		name = $(parameters).eq(i).attr("name");
 		if (name == undefined || name == "undefined") {
 			continue;
@@ -111,7 +150,7 @@ function createMultipart(){
   
   //add parameters
   var parameters = $("#allparameters").find(".realinputvalue");
-	for (i = 0; i < parameters.length; i++) {
+	for (var i = 0; i < parameters.length; i++) {
 		name = $(parameters).eq(i).attr("name");
 		if (name == undefined || name == "undefined") {
 			continue;
@@ -127,4 +166,23 @@ function createMultipart(){
 
 function httpZeroError() {
 	$("#errordiv").append('<div class="alert alert-error"> <a class="close" data-dismiss="alert">&times;</a> <strong>Oh no!</strong> Javascript returned an HTTP 0 error. One common reason this might happen is that you requested a cross-domain resource from a server that did not include the appropriate CORS headers in the response. Better open up your Firebug...</div>');
+}
+
+function initForm(verb, url, params) {
+  var elems = $("#allparameters").find(".httpparameter");
+  for (var i=0; i<elems.length; i++) {
+    elems[i].remove();
+  }
+
+  for (var key in params) {
+    var httpparam = $(".httpparameter:first").clone(true).appendTo("#allparameters");
+    httpparam.find(".fakeinputname").attr("value", key);
+    httpparam.find(".realinputvalue").attr("name", key);
+    httpparam.find(".realinputvalue").attr("value", params[key]);
+  }
+
+  $("#urlvalue").val(url);
+  $("#httpmethod").val(verb);
+
+  showParamHeaders();
 }
